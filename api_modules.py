4fa70ec1fe80fed03288521aeab3277d322f374d
exp-of-betrayal <none@null.0>
2023-04-17 23:09:19 +0400

    addition api

--- api_modules.py
+++ api_modules.py
@@ -1,2 +1,31 @@
-def ...(...: ... = ...) -> ...:
-    ...
+from typing import Dict
+import db
+import json
+from json_schema import JSONSchema
+
+
+def check_auth(id: int, token: str) -> bool:
+    return db.validate_token(id, token)
+
+
+class Api:
+    def __init__(self):
+        self.command_handlers = None
+        self.json_schema = JSONSchema()
+
+    def register_method(self, method: str, handler) -> None:
+        self.command_handlers[method] = handler
+
+    def method_calling(self, data: Dict or json):
+        id = data['id']
+        token = data['token']
+        if not check_auth(id, token):
+            return self.json_schema.generate_error()
+
+
+    def method(self, method_name: str):
+        def wrapper(f):
+            self.register_method(method_name, f)
+            return f
+
+        return wrapper
---
+++ json_schema.py
@@ -0,0 +1,42 @@
+from typing import Dict
+
+
+class JSONSchema:
+    errors = {
+        0: "Произошла неизвестная ошибка.",
+        1: "Передан неизвестный метод.",
+        2: "Авторизация пользователя не удалась.",
+        3: "Слишком много запросов в секунду.",
+        4: "Нет прав для выполнения этого действия.",
+        5: "Произошла внутренняя ошибка сервера.",
+        6: "Доступ запрещён."
+    }
+
+    def generate_error(self,
+                       error_code: int,
+                       request_params: Dict[str, str],
+                       error_description_message: str = None) -> Dict:
+        json_request_params = []
+        for param in request_params.keys():
+            json_request_params.append({
+                "key": param,
+                "value": request_params[param]
+            })
+
+        error = {
+            "error":
+                {
+                    "error_code": error_code,
+                    "error_msg": f"{self.errors[error_code]} : {error_description_message}",
+                },
+            "request_params": json_request_params
+        }
+
+        return error
+
+    def generate_answer(self,
+                        response: Dict):
+        answer = {
+            "response": dict(response)
+        }
+        return answer
